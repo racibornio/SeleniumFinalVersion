38ab12e41d37a2553fcfd6279d01914f7f8e72a9
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2025-01-12 22:24:27 +0100

    Test na FR24.

--- src/test/java/MechBudWebDriverManager.java
+++ src/test/java/MechBudWebDriverManager.java
@@ -3,14 +3,23 @@ import org.bouncycastle.operator.bc.BcSignerOutputStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class MechBudWebDriverManager {
 
+    private static final Logger log = LoggerFactory.getLogger(MechBudWebDriverManager.class);
     static WebDriver chromeDriver;
 
     // dodaj kilka adresow
     String stronaNFM = "https://www.nfm.wroclaw.pl/";
+    String stronaFR24 = "https://www.flightradar24.com/";
 
     @BeforeAll
     static void setupClass() {
@@ -36,7 +45,7 @@ public class MechBudWebDriverManager {
     }
 
     @Test
-    void stronkaNFM() {
+    void testujStronkaNFM() {
         chromeDriver.get(stronaNFM);
         System.out.println("Otwarto strone NFM.");
         // pobierz jej URL
@@ -48,7 +57,42 @@ public class MechBudWebDriverManager {
         } else {
             System.out.println("Adres nie jest poprawny");
         }
-        // == porownuje referenjce - adresy w pamieci - czy sa/jest identyczne/y dla obiektow
+        // == porownuje referencje - adresy w pamieci - czy sa/jest identyczne/y dla obiektow
         // .equels() porownuje wartosc obiektu i/lub zmiennej
+    }
+
+    @Test
+    void tesujStronkaFR24() {
+        chromeDriver.get(stronaFR24);
+        System.out.println("Stronka trackera lotow otwarta");
+        String tytulFR24 = chromeDriver.getTitle();
+        System.out.println("Tytul to " + tytulFR24);
+
+        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+        WebElement rejectCookiesBtn = chromeDriver.findElement(By.id("onetrust-reject-all-handler"));
+        rejectCookiesBtn.click();
+        System.out.println("Cookiesy odrzucone");
+
+        //WebElement logInBtn = chromeDriver.findElement(By.className("flex h-9 flex-col items-center justify-center text-white"));
+        //logInBtn.click();
+        //System.out.println("Button logIn klikniety");
+        // Compound class names not permitted
+
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+        WebElement logInBtn = chromeDriver.findElement(By.cssSelector(".flex.h-9.flex-col.items-center.justify-center.text-white"));
+        logInBtn.click();
+        System.out.println("Button logIn klikniety");
+
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+
+        WebElement emailField = chromeDriver.findElement(By.className("email"));
+        emailField.sendKeys("ostrowski.patryk@gmail.com");
+        WebElement pwdField = chromeDriver.findElement(By.name("props.name"));
+        pwdField.sendKeys("t3ra2POLSKA!");
+        WebElement performLoginBtn = chromeDriver.findElement(By.cssSelector("[data-testid='login__submit-button']"));
+        performLoginBtn.click();
+        System.out.println("Great success!");
+        //t3ra2POLSKA!
+
     }
 }
